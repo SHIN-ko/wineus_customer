8993157b9dafc0d7ae367c773a8eef8e946998f6
Andreas <shs2810@gmail.com>
2021-01-18 00:13:41 +0900

    post delete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import PostListPage from './pages/post/PostListPage';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import WritePage from './pages/post/WritePage';
-import PostPage from './pages/post/PostPage';
 import CustomerListPage from './pages/customer/CustomerListPage';
 import CustomerRegisterPage from './pages/customer/CustomerRegisterPage';
 import CustomerPage from './pages/customer/CustomerPage';
